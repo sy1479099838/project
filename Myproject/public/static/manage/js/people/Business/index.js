c7bacc35fbcfb52ee1a4dce9bb704e3f04e6f008
shenrenhai <1294544527@qq.com>
2017-12-06 17:31:13 +0800

    修改

--- Myproject/public/static/manage/js/people/Business/index.js
+++ Myproject/public/static/manage/js/people/Business/index.js
@@ -29,3 +29,25 @@ $(function () {
 function closeImage() {
     $(".SeeBigImages").fadeOut(0);
 }
+
+
+
+function closeAddbuinsess() {
+    $(".Pcmenu-AddMenu-Box").fadeOut(1000);
+}
+function Addbuinsess() {
+    $(".Pcmenu-AddMenu-Box").fadeIn(700);
+}
+
+function preview(file) {
+    var prevDiv = document.getElementById('preview');
+    if (file.files && file.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(evt) {
+            prevDiv.innerHTML = '<img src="' + evt.target.result + '" />';
+        }
+        reader.readAsDataURL(file.files[0]);
+    } else {
+        prevDiv.innerHTML = '<div class="img" style="filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale,src=\'' + file.value + '\'"></div>';
+    }
+}
