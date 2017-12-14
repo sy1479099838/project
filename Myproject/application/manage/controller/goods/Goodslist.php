59c679dd07472f956bec6a037180b85e884cdbbb
shenrenhai <1294544527@qq.com>
2017-12-14 15:49:19 +0800

    商品分类查询

--- Myproject/application/manage/controller/goods/Goodslist.php
+++ Myproject/application/manage/controller/goods/Goodslist.php
@@ -1,10 +1,14 @@
 <?php
 namespace app\manage\controller\goods;
 use app\manage\controller\Common;
+use app\manage\model\GoodsClassify;
 class Goodslist extends Common
 {
     public function index()
     {
+        $ClassList=GoodsClassify::where("Enable","1")->select();
+        $ClassList=$this->treeData(json_decode(json_encode($ClassList,true),true));
+        $this->assign("ClassList",$ClassList);
         return $this->fetch();
     }
     public function treeData($data,$pid = 0){
