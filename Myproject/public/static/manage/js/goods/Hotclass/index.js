a6a97b643cba6ddb7c633d3a7544b9ca9976af78
Administrator <1294544527@qq.com>
2017-12-16 18:32:25 +0800

    热商品分类页面

---
+++ Myproject/public/static/manage/js/goods/Hotclass/index.js
@@ -0,0 +1,68 @@
+
+$(document).ready(function(){
+
+    $(".PcMenu-Enable").each(function(){
+        // alert($(this).children(".status").val());
+        if($(this).children(".status").val()=="1")
+        {
+            $(this).children(".PcMenu-open").fadeIn();
+            $(this).children(".PcMenu-off").fadeOut();
+        }
+        else if($(this).children(".status").val()!="1")
+        {
+            $(this).children(".PcMenu-off").fadeIn();
+            $(this).children(".PcMenu-open").fadeOut();
+        }
+    })
+
+});
+
+function OffMenua(date) {
+    $.ajax({
+        type: "POST",
+        url: 'OffRegion',
+        data:({id:date}),
+        success: function(msg){
+            if(msg==="success")
+            {
+                $.showBox("操作成功！");
+                // alert(date);
+                $(this).children(".PcMenu-off").fadeIn();
+                $(this).children(".PcMenu-open").fadeOut();
+            }
+        },
+        error:function (err){
+            $.showBox("失败，请重试！");
+        }
+    });
+}
+function OpenMenua(date) {
+    $.ajax({
+        type: "POST",
+        url: 'OpenRegion',
+        data:({id:date}),
+        success: function(msg){
+            if(msg==="success")
+            {
+                $.showBox("操作成功！");
+                // alert(date);
+                $(this).children(".PcMenu-open").fadeIn();
+                $(this).children(".PcMenu-off").fadeOut();
+            }
+        },
+        error:function (err){
+            $.showBox("失败，请重试！");
+        }
+    });
+}
+
+    function Add_fenlei_re(){
+        //alert(11111111);
+        $(".Add_fenlei_re").fadeIn();
+    }
+
+    function closeAdd_fenlei_re(){
+        $(".Add_fenlei_re").fadeOut();
+    }
+
+
