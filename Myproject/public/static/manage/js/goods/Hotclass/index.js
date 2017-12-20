7a8e8b62fa602f01fa3b4137605965f25ffa0b06
Administrator <1294544527@qq.com>
2017-12-20 10:32:32 +0800

    热门商品分类编辑

--- Myproject/public/static/manage/js/goods/Hotclass/index.js
+++ Myproject/public/static/manage/js/goods/Hotclass/index.js
@@ -99,7 +99,7 @@ function OpenHotClass(date) {
         $(".edit_fenlei_re").fadeOut();
     }
 
-/*分类图标预览*/
+/*添加分类图标预览*/
 function previewFile () {
     var imgfile = document.getElementById("u_img").files[0];
     var prew=document.getElementById("img_1");
@@ -178,83 +178,159 @@ function Add_fenlei_re_submit() {
         });
     }
 }
+/*编辑分类图标预览*/
+function previewFile_1 () {
+    var imgfile = document.getElementById("u_img_1").files[0];
+    var prew=document.getElementById("img_2");
+    var reader = new FileReader();
+    reader.readAsDataURL(imgfile);
+    reader.onload= function (theFile) {
+        var image = new Image();
+        image.src = theFile.target.result;
+        image.onload = function() {
+            if (this.width==40 && this.height==40) {
+                prew.src=image.src;
+            } else {
+                imgfile.src = "";
+                document.getElementById("u_img").value="";
+                $.showBox("请上传40*40的图标");
+            }
+        };
+    };
 
+
+}
 
 /*热门商品分类编辑*/
 function Add_fenlei_re_Edit(){
 
     var fd = new FormData();
     fd.append("upload", 1);
-    fd.append("upfile", $("#img3").get(0).files[0]);
-    $.ajax({
-        url: "UplodeImg",
-        type: "POST",
-        processData: false,
-        contentType: false,
-        data: fd,
-        success: function(msg) {
-            if(msg=="error")
-            {
-                $.showBox("照片上传失败！");
-            }
-            else
-            {
-                var  Name=$("input[name='fenlei_min']").val();
-                var  Muban=$("input[name='fenlei_mu']").val();
-                var  Kaiguan=$("input[name='kaiguan']").val();
-                var  Xu=$("input[name='fenlei_xu']").val();
-                var  Head=msg;
-
-
-                if(Name=="")
+    fd.append("upfile", $("#u_img_1").get(0).files[0]);
+    var pan_photo=fd.append("upfile", $("#u_img_1").get(0).files[0]);
+    if(pan_photo!=''){
+        $.ajax({
+            url: "UplodeImg",
+            type: "POST",
+            processData: false,
+            contentType: false,
+            data: fd,
+            success: function(msg) {
+                if(msg=="error")
                 {
-                    $.showBox('请输入分类名称');
-                }else if(Muban=="")
-                {
-                    $.showBox("请输入所用模板");
-                }else if(Kaiguan==""){
-                    $.showBox("请明确是否启用");
-                }else if(Xu=="")
-                {
-                    $.showBox ("请填写排列序列");
+                    $.showBox("照片上传失败！");
                 }
-                else{
-
-                    $.ajax({
-                        type:'post',
-                        url:'AddBusiness',
-                        data:({
-                            Name:name,
-                            Muban:Muban,
-                            Kaiguan:Kaiguan,
-                            Xu:Xu,
-                            Head:Head
-                        }),
-                        success:function(data){
-                            if(data=="success")
-                            {
-                                $.showBox("热门商品分类添加成功！");
-                                window.location.reload();
-                            }
-                            else if(data=="error")
-                            {
-                                $.showBox("热门商品分类添加失败！");
-                                window.location.reload();
-                            }
-                            else
-                            {
-                                $.showBox(data);
-                            }
-                        }
+                else
+                {
+                    var  Name=$("input[name='edit_fenlei_min']").val();
+                    var  Muban=$("input[name='edit_moban']").val();
+                    var  Kaiguan=$("input[name='Editkaiguan']").val();
+                    var  Xu=$("input[name='edit_xu']").val();
+                    var  Head=msg;
 
 
-                    });
+                    if(Name=="")
+                    {
+                        $.showBox('请输入分类名称');
+                    }else if(Muban=="")
+                    {
+                        $.showBox("请输入所用模板");
+                    }else if(Kaiguan=="")
+                    {
+                        $.showBox("请明确是否启用");
+                    }else if(Xu=="")
+                    {
+                        $.showBox ("请填写排列序列");
+                    }
+                    else{
+
+                        $.ajax({
+                            type:'post',
+                            url:'AddBusiness',
+                            data:({
+                                Name:name,
+                                Muban:Muban,
+                                Kaiguan:Kaiguan,
+                                Xu:Xu,
+                                Head:Head
+                            }),
+                            success:function(data){
+                                if(data=="success")
+                                {
+                                    $.showBox("热门商品分类添加成功！");
+                                    window.location.reload();
+                                }
+                                else if(data=="error")
+                                {
+                                    $.showBox("热门商品分类添加失败！");
+                                    window.location.reload();
+                                }
+                                else
+                                {
+                                    $.showBox(data);
+                                }
+                            }
+
+
+                        });
+                    }
+
+
                 }
-
-
             }
+        });
+    }else{
+        var  Name=$("input[name='edit_fenlei_min']").val();
+        var  Muban=$("input[name='edit_moban']").val();
+        var  Kaiguan=$("input[name='Editkaiguan']").val();
+        var  Xu=$("input[name='edit_xu']").val();
+
+        if(Name=="")
+        {
+            $.showBox('请输入分类名称');
+        }else if(Muban=="")
+        {
+            $.showBox("请输入所用模板");
+        }else if(Kaiguan=="")
+        {
+            $.showBox("请明确是否启用");
+        }else if(Xu=="")
+        {
+            $.showBox ("请填写排列序列");
         }
-    });
+        else{
+
+            $.ajax({
+                type:'post',
+                url:'AddBusiness',
+                data:({
+                    Name:name,
+                    Muban:Muban,
+                    Kaiguan:Kaiguan,
+                    Xu:Xu
+                }),
+                success:function(data){
+                    if(data=="success")
+                    {
+                        $.showBox("热门商品分类添加成功！");
+                        window.location.reload();
+                    }
+                    else if(data=="error")
+                    {
+                        $.showBox("热门商品分类添加失败！");
+                        window.location.reload();
+                    }
+                    else
+                    {
+                        $.showBox(data);
+                    }
+                }
+
+
+            });
+        }
+    }
+
 }
 
 
