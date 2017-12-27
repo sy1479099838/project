c00cd59945daf427c4d354e4f081439cfe519b8f
shenrenhai <1294544527@qq.com>
2017-12-27 15:54:22 +0800

    距离查询

--- Myproject/application/admin/controller/home/Home.php
+++ Myproject/application/admin/controller/home/Home.php
@@ -9,6 +9,33 @@ class Home extends Common
 {
     public function index()
     {
+//根据距离查询距离并排序
+//        $qwe="ROUND(
+//        6378.138 * 2 * ASIN(
+//            SQRT(
+//                POW(
+//                    SIN(
+//                        (
+//                            31.472743 * PI() / 180 - Y_LONG * PI() / 180
+//                        ) / 2
+//                    ),
+//                    2
+//                ) + COS(31.472743 * PI() / 180) * COS(Y_LONG * PI() / 180) * POW(
+//                    SIN(
+//                        (
+//                            104.750124 * PI() / 180 -   X_LONG * PI() / 180
+//                        ) / 2
+//                    ),
+//                    2
+//                )
+//            )
+//        ) * 1000
+//    )";
+//        $juli=Goods::where("id","neq","0")
+//            ->field("X_LONG,Y_LONG,id,GoodsName,Template_1,Template_2,Template_3,Template_4,startTime,endTime,".$qwe."as juli")
+//            ->order($qwe,"DESC")
+//            ->select();
+//        dump(json_decode(json_encode($juli,true),true));exit;
         $GoodsList=json_decode(json_encode(Hotclass::where("enable","1")->order(['order','id'=>'desc'])->select(),true),true);
         foreach ($GoodsList as $key=>$val)
         {
