35577555399575eb917c2a3c483dff86b0904796
shenrenhai <1294544527@qq.com>
2018-01-03 15:06:34 +0800

    微信登录

--- Myproject/application/manage/controller/weixin/Createmenu.php
+++ Myproject/application/manage/controller/weixin/Createmenu.php
@@ -7,13 +7,12 @@ class Createmenu extends Common
     {
 
         $cacessToken=Common::getAccessToken();
-
 //        $url="https://api.weixin.qq.com/cgi-bin/menu/get?access_token=$cacessToken";
         $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=$cacessToken";
 
         $data='{
                  "button":[
-                 {
+                 {    
                       "type":"click",
                       "name":"今日最热",
                       "key":"V1001_TODAY_MUSIC"
@@ -21,10 +20,10 @@ class Createmenu extends Common
                   {
                        "name":"旅游",
                        "sub_button":[
-                       {
+                       {    
                            "type":"view",
                            "name":"商城",
-                           "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx128a1dc9cf653ec1&redirect_uri=http://www.hfyiqiwan.top/admin/login/Weixin/index&response_type=code&scope=snsapi_userinfo&state=shenyang#wechat_redirec"
+                           "url":"http://www.hfyiqiwan.top"
                         },
                         {
                            "type":"click",
@@ -34,9 +33,9 @@ class Createmenu extends Common
                    }]
              }';
         $menuList=Common::http_request($url,"post" ,$data);
-//        $menuList=json_decode($menuList,true);
-//        $this->assign("count",count($menuList["menu"]["button"]));
-//        $this->assign("menuList",$menuList["menu"]["button"]);
-//        return $this->fetch();
+        $menuList=json_decode($menuList,true);
+        $this->assign("count",count($menuList["menu"]["button"]));
+        $this->assign("menuList",$menuList["menu"]["button"]);
+        return $this->fetch();
     }
 }
