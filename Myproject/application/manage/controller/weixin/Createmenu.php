38ab19fb2e84900c5b616535b978212931601aa2
shenrenhai <1294544527@qq.com>
2018-01-03 15:04:40 +0800

    微信登录

--- Myproject/application/manage/controller/weixin/Createmenu.php
+++ Myproject/application/manage/controller/weixin/Createmenu.php
@@ -7,35 +7,36 @@ class Createmenu extends Common
     {
 
         $cacessToken=Common::getAccessToken();
-        $url="https://api.weixin.qq.com/cgi-bin/menu/get?access_token=$cacessToken";
-//        $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=$cacessToken";
 
-//        $data='{
-//                 "button":[
-//                 {
-//                      "type":"click",
-//                      "name":"今日最热",
-//                      "key":"V1001_TODAY_MUSIC"
-//                  },
-//                  {
-//                       "name":"旅游",
-//                       "sub_button":[
-//                       {
-//                           "type":"view",
-//                           "name":"商城",
-//                           "url":"http://www.hfyiqiwan.top"
-//                        },
-//                        {
-//                           "type":"click",
-//                           "name":"赞一下我们",
-//                           "key":"您好"
-//                        }]
-//                   }]
-//             }';
-        $menuList=Common::http_request($url);
-        $menuList=json_decode($menuList,true);
-        $this->assign("count",count($menuList["menu"]["button"]));
-        $this->assign("menuList",$menuList["menu"]["button"]);
-        return $this->fetch();
+//        $url="https://api.weixin.qq.com/cgi-bin/menu/get?access_token=$cacessToken";
+        $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=$cacessToken";
+
+        $data='{
+                 "button":[
+                 {
+                      "type":"click",
+                      "name":"今日最热",
+                      "key":"V1001_TODAY_MUSIC"
+                  },
+                  {
+                       "name":"旅游",
+                       "sub_button":[
+                       {
+                           "type":"view",
+                           "name":"商城",
+                           "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx128a1dc9cf653ec1&redirect_uri=http://www.hfyiqiwan.top/admin/login/Weixin/index&response_type=code&scope=snsapi_userinfo&state=shenyang#wechat_redirec"
+                        },
+                        {
+                           "type":"click",
+                           "name":"赞一下我们",
+                           "key":"您好"
+                        }]
+                   }]
+             }';
+        $menuList=Common::http_request($url,"post" ,$data);
+//        $menuList=json_decode($menuList,true);
+//        $this->assign("count",count($menuList["menu"]["button"]));
+//        $this->assign("menuList",$menuList["menu"]["button"]);
+//        return $this->fetch();
     }
 }
