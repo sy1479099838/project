6061633c99b34b839cf702c35a8244d956628bcc
zsj <964311356@QQ.com>
2018-01-04 16:14:14 +0800

    微信公众号餐单

--- Myproject/public/static/manage/js/weixin/Createmenu/index.js
+++ Myproject/public/static/manage/js/weixin/Createmenu/index.js
@@ -21,8 +21,38 @@ function Add_menu_list(){
             var menu='<div class="Add_menu_list_1" id="A'+num+'">餐单名称：<input type="text" name="WX_name" value="" onchange="check_name()">' +
             '&nbsp;&nbsp;&nbsp;启用：<input type="radio" name="qiyong" value="1" style="width:5%;" onchange="qi_yong('+num+')">' +
             '关闭：<input type="radio" name="qiyong" value="0" checked style="width:5%;" onchange="guan_bi('+num+')"></div>';
-            $('.jiange').after(menu);
+            $('#jiange').after(menu);
         }else{
+        $.showBox('最多只能有5个二级餐单');
+    }
+
+}
+function Add_menu_list2(){
+
+    var num=document.getElementsByClassName('Add_menu_list_2').length;
+    //alert(num);
+    if(num<5)
+    {
+        var menu='<div class="Add_menu_list_2" id="A1'+num+'">餐单名称：<input type="text" name="WX_name" value="" onchange="check_name()">' +
+            '&nbsp;&nbsp;&nbsp;启用：<input type="radio" name="qiyong" value="1" style="width:5%;" onchange="qi_yong2('+num+')">' +
+            '关闭：<input type="radio" name="qiyong" value="0" checked style="width:5%;" onchange="guan_bi2('+num+')"></div>';
+        $('#jiange2').after(menu);
+    }else{
+        $.showBox('最多只能有5个二级餐单');
+    }
+
+}
+function Add_menu_list3(){
+
+    var num=document.getElementsByClassName('Add_menu_list_3').length;
+    //alert(num);
+    if(num<5)
+    {
+        var menu='<div class="Add_menu_list_3" id="A2'+num+'">餐单名称：<input type="text" name="WX_name" value="" onchange="check_name()">' +
+            '&nbsp;&nbsp;&nbsp;启用：<input type="radio" name="qiyong" value="1" style="width:5%;" onchange="qi_yong3('+num+')">' +
+            '关闭：<input type="radio" name="qiyong" value="0" checked style="width:5%;" onchange="guan_bi3('+num+')"></div>';
+        $('#jiange3').after(menu);
+    }else{
         $.showBox('最多只能有5个二级餐单');
     }
 
@@ -47,8 +77,32 @@ function qi_yong(num){
 
 
 }
+function qi_yong2(num){
+    //alert(111111111);
+    var ermenu='<div class="er_Menu" id="B2'+num+'">跳转链接：<input type="radio" name="tiaozhan" value="0" style="width:2%;" onchange="Add_lianjie2('+num+')" class="lianjie">'+
+        '点击事件:<input type="radio" name="tiaozhan" value="1" style="width:2%;" onclick="Add_sj('+num+')" class="tianjie_e"></div>'
+    ;
+    $('#A1'+num).append(ermenu);
+
+
+}
+function qi_yong3(num){
+    //alert(111111111);
+    var ermenu='<div class="er_Menu" id="B3'+num+'">跳转链接：<input type="radio" name="tiaozhan" value="0" style="width:2%;" onchange="Add_lianjie3('+num+')" class="lianjie">'+
+        '点击事件:<input type="radio" name="tiaozhan" value="1" style="width:2%;" onclick="Add_sj('+num+')" class="tianjie_e"></div>'
+    ;
+    $('#A2'+num).append(ermenu);
+
+
+}
 function guan_bi(num){
     $('#A'+num).remove();
+}
+function guan_bi2(num){
+    $('#A1'+num).remove();
+}
+function guan_bi3(num){
+    $('#A2'+num).remove();
 }
 function Add_lianjie(num ){
     //alert(11111111);
@@ -56,22 +110,20 @@ function Add_lianjie(num ){
     $('#B'+num).append(lianjie_1);
     $('#D'+num).remove();
 }
+function Add_lianjie2(num ){
+    //alert(11111111);
+    var lianjie_1='<input type="text" name="lianjie" value="" class="lianjie_1" style="width:30%;border:1px solid black;" id="C'+num+'">';
+    $('#B2'+num).append(lianjie_1);
+    $('#D2'+num).remove();
+}
+function Add_lianjie3(num ){
+    //alert(11111111);
+    var lianjie_1='<input type="text" name="lianjie" value="" class="lianjie_1" style="width:30%;border:1px solid black;" id="C'+num+'">';
+    $('#B3'+num).append(lianjie_1);
+    $('#D3'+num).remove();
+}
 function Add_sj(num){
     $('#C'+num).remove();
 }
-function Add_er(num){
-    var erji='<button class="Add_er_1" onclick="Add_er_1('+num+')" id="D'+num+'">点击此处添加餐单</button>';
-    $('#B'+num).append(erji);
-    $('#C'+num).remove();
-}
 
-function Add_er_1(num){
-    var num_san=document.getElementsByClassName('sanji_1').length;
-    if(num_san<7){
-        var  sanji='<div class="sanji_1">请输入三级餐单名称<input name="sanji" type="text" value="" class="sanji_candan"></div><br>';
-        $('#D'+num).after(sanji);
-    }else{
-        $.showBox('三级餐单只能有7个');
-    }
 
-}
