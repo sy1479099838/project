7f25ef7eb91960e66df6f16ed6d9459ae57fd262
zsj <964311356@QQ.com>
2018-01-08 10:35:51 +0800

    微信公众套餐

--- Myproject/public/static/admin/js/goods/Details/index.js
+++ Myproject/public/static/admin/js/goods/Details/index.js
@@ -270,20 +270,23 @@ $(function (){
     };
 
     var lis=document.getElementById('Tao_second_2');
-    var Ole=lis.getElementsByTagName('li');
+    var Ole=lis.getElementsByTagName('div');
     var num=0;
     var Oldli=null;
     Oldli=Ole[num];
-    console.log(Ole);
+    //console.log(Ole);
     //Ole[num].className='oldclass';
     for(var i=0;i<Ole.length;i++){
         Ole[i].onclick=function(){
             //console.log(Ole[i]);
             Oldli.className="active";
-            console.log(Oldli);
+            Oldli=this;
             this.className="l_change";
+            console.log(Oldli);
+
 
         };
+
 
     }
 });
