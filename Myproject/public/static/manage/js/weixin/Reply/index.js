9d8fea414ebf091b358cd596e5ef05b0e1d917db
zsj <964311356@QQ.com>
2018-01-11 17:30:40 +0800

    购物车样式修改

--- Myproject/public/static/manage/js/weixin/Reply/index.js
+++ Myproject/public/static/manage/js/weixin/Reply/index.js
@@ -1,3 +1,21 @@
+
+
+function Huifu(Names){
+    //alert(111111111);
+    for(i=1;i<4;i++){
+        var tempname="mune_x"+i;
+        var NewsHot="x"+i;	// “X”是ID名称，与上面的传值的|数值匹配
+        if (Names==tempname){
+            Nnews=document.getElementById(NewsHot)
+            Nnews.style.display='';
+        }else{
+            Nnews=document.getElementById(NewsHot)
+            Nnews.style.display='none';
+        }
+    }
+
+}
+
 function AddReply(){
 
 }
