281092e878f01b9d8e26a5ca1e3f0ef56e508825
zsj <964311356@QQ.com>
2018-01-15 15:49:35 +0800

    订单确认

--- Myproject/public/static/admin/js/personal/Personal/orderform.js
+++ Myproject/public/static/admin/js/personal/Personal/orderform.js
@@ -2,24 +2,52 @@ function getId(v){return document.getElementById(v);}
 var x=0;
 var i=0;
 function clc(y,data){
-    $("#order"+y).removeAttr('onclick').attr("onclick","clc("+y+","+(data+1)+");");
     var num=data%3;
     if(num==0)
     {
         alert("可以传值");
+        $.ajax({
+            url:"check_all",
+            data:({
+                y:y
+            }),
+            type:"post",
+            success:function (msg) {
+                if(msg=="sucess"){
+                    document.body.style.overflow="hidden";
+                    getId("Ding"+x).style.display = "none";
+                    getId("Ding"+y).style.display = "";
+                    x=y;
+                    var lis=document.getElementById('Tao_second_2');
+                    var Ole=lis.getElementsByTagName('div');
+                    for( var i=0;i<Ole.length;i++){
+                        Ole[i].className="active";
+                        Ole[y].className="l_change";
+                    }
+                    $("#order"+y).removeAttr('onclick').attr("onclick","clc("+y+","+(data+1)+");");
+                }
+
+            },
+            error:function (msg) {
+                //alert("请刷新页面");
+            }
+        })
     }
     else
     {
-        alert("对不起，暂时不能传值");
+        //alert("对不起，暂时不能传值");
+        document.body.style.overflow="hidden";
+        getId("Ding"+x).style.display = "none";
+        getId("Ding"+y).style.display = "";
+        x=y;
+        var lis=document.getElementById('Tao_second_2');
+        var Ole=lis.getElementsByTagName('div');
+        for( var i=0;i<Ole.length;i++){
+            Ole[i].className="active";
+            Ole[y].className="l_change";
+        }
+        $("#order"+y).removeAttr('onclick').attr("onclick","clc("+y+","+(data+1)+");");
     }
-    document.body.style.overflow="hidden";
-    getId("Ding"+x).style.display = "none";
-    getId("Ding"+y).style.display = "";
-    x=y;
-    var lis=document.getElementById('Tao_second_2');
-    var Ole=lis.getElementsByTagName('div');
-    for( var i=0;i<Ole.length;i++){
-        Ole[i].className="active";
-        Ole[y].className="l_change";
-    }
+
+
 }
