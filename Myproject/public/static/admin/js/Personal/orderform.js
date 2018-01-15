4a6d8880eb637288ec84548c4598c7b96197322b
shenrenhai <1294544527@qq.com>
2018-01-15 14:26:49 +0800

    订单查询

--- Myproject/public/static/admin/js/Personal/orderform.js
+++ Myproject/public/static/admin/js/Personal/orderform.js
@@ -20,3 +20,20 @@
 //
 //     }
 // });
+
+document.body.style.overflow="hidden";
+function $(v){return document.getElementById(v);}
+var x=0;
+var i=0;
+function c(y){
+    $("Ding"+x).style.display = "none";
+    $("Ding"+y).style.display = "";
+    x=y;
+    var lis=document.getElementById('Tao_second_2');
+    var Ole=lis.getElementsByTagName('div');
+    for( var i=0;i<Ole.length;i++){
+        Ole[i].className="active";
+        Ole[y].className="l_change";
+    }
+
+}
