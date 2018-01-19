6060e1065217d5f9db3d17dffdb98410e4891922
zsj <964311356@QQ.com>
2018-01-19 14:21:12 +0800

    拼团

---
+++ Myproject/public/static/admin/js/personal/Personal/address.js
@@ -0,0 +1,18 @@
+function guan_li(){
+    $(".icon-edit").fadeIn();
+    $(".icon-trash").fadeIn();
+    $(".Add_button").fadeOut();
+    $(".Add_button2").fadeIn();
+}
+function guan_li_close(){
+    $(".icon-edit").fadeOut();
+    $(".icon-trash").fadeOut();
+    $(".Add_button2").fadeOut();
+    $(".Add_button").fadeIn();
+}
+function Add_ress(){
+    $(".Address_hidden").fadeIn();
+}
+function Add_ress_close(){
+    $(".Address_hidden").fadeOut();
+}
