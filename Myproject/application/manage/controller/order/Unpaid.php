d22c6686a70248bcfac3a50efcbbd9604894c704
shenrenhai <1294544527@qq.com>
2018-01-22 12:04:16 +0800

    未支付订单查询

--- Myproject/application/manage/controller/order/Unpaid.php
+++ Myproject/application/manage/controller/order/Unpaid.php
@@ -1,12 +1,23 @@
 <?php
 namespace app\manage\controller\order;
 use app\manage\controller\Common;
+use app\admin\model\GoodsOrder;
+use think\Db;
 class Unpaid extends Common
 {
     public function index()
     {
-        $this->assign("Title","未支付订单");
-        $this->assign("JsName","order/Unpaid/index");
+        $order=Db::name('goods_order')->where('state',0)
+            ->alias('a')
+            ->join('goods b','a.GoodsId = b.id')
+            ->join('goods_package c','a.PackageId = c.PackageId')
+            ->join("user d","a.User = d.id")
+            ->field("
+                a.GoodsOrderID,a.num,a.lastUpdateTime,a.ReceiveAddress,a.price,a.ReceivePeople,a.ReceivePhoneNum,
+                b.GoodsName,c.PackageName,d.UserName
+            ")
+            ->select();
+        $this->assign("order",$order);
         return $this->fetch();
     }
 }
