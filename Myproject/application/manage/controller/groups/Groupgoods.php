8a0c8752984932035916da03fca893753e96c2c3
zsj <964311356@QQ.com>
2018-01-23 19:05:00 +0800

    微信公众号菜单

---
+++ Myproject/application/manage/controller/groups/Groupgoods.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\manage\controller\groups;
+use app\manage\controller\Common;
+class Groupgoods extends Common
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+}
--- Myproject/public/static/manage/js/weixin/Reply/index.js
+++ Myproject/public/static/manage/js/weixin/Reply/index.js
@@ -21,3 +21,35 @@ function Appaly_close(){
 function AddReply(){
     $(".Appaly_box").fadeIn();
 }
+function AddguanReply(){
+    $(".Gujian_h").fadeIn();
+}
+function Gujian_h_close(){
+    $(".Gujian_h").fadeOut();
+}
+
+
+
+function guanjiansub(){
+    var form = new FormData(document.getElementById("huifu"));
+    $.ajax({
+        url:'guanjianzi',
+        type:"post",
+        data:form,
+        processData:false,
+        contentType:false,
+        async:false,
+        success:function (msg) {
+            if(msg!=="error"){
+                $.showBox("添加成功");
+                //alert("1111111");
+            }else{
+                $.showBox("添加失败");
+            }
+
+        },
+        error:function (msg) {
+            $.showBox("添加失败");
+        }
+    });
+}
