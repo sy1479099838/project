71d41db222a134ddc91b2bc4e2bf6091b03fd1c1
zsj <964311356@QQ.com>
2018-01-24 18:57:30 +0800

    微信公众号菜单

--- Myproject/public/static/manage/js/weixin/Createmenu/index.js
+++ Myproject/public/static/manage/js/weixin/Createmenu/index.js
@@ -19,6 +19,27 @@ function Baocun(){
     for(i=0;i<minlist.length;i++){
         minlist[i].disabled="disabled";
     }
+    var form = new FormData(document.getElementById("tijiao"));
+    $.ajax({
+        url:'caidantijiao',
+        type:"post",
+        data:form,
+        processData:false,
+        contentType:false,
+        async:false,
+        success:function (msg) {
+            if(msg!=="error"){
+                $.showBox("提交成功");
+                alert("1111111");
+            }else{
+                $.showBox("提交失败");
+            }
+
+        },
+        error:function (msg) {
+            $.showBox("提交成功");
+        }
+    });
 }
 
 
@@ -96,3 +117,81 @@ function zidianji(menu,num){
     }
 }
 
+function qiyong(menu,num)
+{
+    if(num==0){
+        var list='<div class="wei_menu" id="ziweixinmenu_'+menu+'"><div class="ziwei_menu3">' +
+            '餐单名称：<input type="text" value="" placeholder="餐单名称不能超过4个汉字，8个字符" name="candan_'+menu+'" style="width:60%;text-align:center;" > ' +
+            '</div>' +
+            '<div class="ziwei_menu2"> ' +
+            '<span>跳转链接<input type="radio" name="dianji'+menu+'" value="0" onchange="zikaiguan(1,0)"></span> ' +
+            '<span>点击事件<input type="radio" name="dianji'+menu+'" value="1" onchange="zikaiguan(1,1)"></span>' +
+            '</div>' +
+            '<div class="ziwei_menu4" onclick="addzi('+menu+','+num+')">添加子菜单</div></div>';
+        $("#weixinmenu_"+menu).after(list);
+
+    }
+    if(num==1){
+        $('#ziweixinmenu_'+menu).remove();
+    }
+}
+
+function zikaiguan(menu,num){
+    if(num==0){
+        $('.wei_menu'+menu).remove();
+        var list='<div class="wei_menu" id="zilist_'+menu+'">' +
+            '<input name="lianjie'+menu+'" value="" placeholder="http://" class="lianjie_shuru">' +
+            '</div><br>';
+        $("#ziweixinmenu_"+menu).after(list);
+    }else{
+        $('#zilist_'+menu).remove();
+    }
+    if(num==1){
+        $('.wei_menu'+menu).remove();
+        $("zi_caidan"+menu).remove();
+        var list='<div class="wei_menu" id="zitiaozhuan_'+menu+'">' +
+            '<input name="chufa'+menu+'" value="" placeholder="添加点击事件" class="lianjie_shuru">' +
+            '</div><br>';
+        $("#ziweixinmenu_"+menu).after(list);
+    }else{
+        $('#zitiaozhuan_'+menu).remove();
+    }
+}
+
+function addzi(menu,num){
+    $('#zilist_'+menu).remove();
+    $('#zitiaozhuan_'+menu).remove();
+    var shuzi=document.getElementsByClassName('wei_menu1').length;
+    var list='<div class="wei_menu'+menu+'" id="zi_caidan'+menu+shuzi+'">' +
+        '<div class="ziwei_menu3">' +
+        '子餐单名称：<input type="text" value="" placeholder="餐单名称不能超过4个汉字，8个字符" name="zicandan_'+menu+'" style="width:60%;text-align:center;" > ' +
+        '</div>' +
+        '<div class="ziwei_menu2"> ' +
+        '<span>跳转链接<input type="radio" name="zidianji'+menu+'" value="0" onchange="zilist('+menu+shuzi+',0)"></span> ' +
+        '<span>点击事件<input type="radio" name="zidianji'+menu+'" value="1" onchange="zilist('+menu+shuzi+',1)"></span>' +
+        '</div>' +
+        '</div>';
+    if(shuzi<5){
+        $("#ziweixinmenu_"+menu).after(list);
+    }else{
+        alert('只能添加5个自惨淡');
+    }
+
+}
+
+function zilist(nemu,num){
+    var shuzi=document.getElementsByClassName('wei_menu1').length;
+    console.log(nemu);
+    if(num==0){
+        var list='<input name="lianjie'+nemu+shuzi+'" value="" placeholder="http://" class="lianjie_shuru2" id="zhebushi'+nemu+shuzi+'">';
+        $('#zi_caidan'+nemu).append(list);
+    }else{
+        $('#zhebushi'+nemu+shuzi).remove();
+    }
+    if(num==1){
+        var list='<input name="lianjie'+nemu+shuzi+'" value="" placeholder="触发事件" class="lianjie_shuru2" id="zheshi'+nemu+shuzi+'">';
+        $('#zi_caidan'+nemu).append(list);
+    }else{
+        $('#zheshi'+nemu+shuzi).remove();
+    }
+}
