74053dc38ae8f09dbc4f8915575e1a386ebd95dc
shenrenhai <1294544527@qq.com>
2018-01-31 10:37:26 +0800

    细节修改

--- Myproject/application/manage/controller/weixin/Createmenu.php
+++ Myproject/application/manage/controller/weixin/Createmenu.php
@@ -5,50 +5,10 @@ class Createmenu extends Common
 {
     public function index()
     {
-
         $cacessToken=Common::getAccessToken();
         $url="https://api.weixin.qq.com/cgi-bin/menu/get?access_token=$cacessToken";
-//        $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=$cacessToken";
-
-//        $data='{
-//                 "button":[
-//                 {
-//                      "type":"view",
-//                           "name":"一起玩",
-//                           "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxdfb8f36560c95e58&redirect_uri=http://www.hfyiqiwan.top/admin/login/Weixin/index&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
-//                  },
-//                  {
-//                       "name":"黄蜂福利",
-//                       "sub_button":[
-//                       {
-//                           "type":"view",
-//                           "name":"百度搜索",
-//                           "url":"https://www.baidu.com"
-//                        },
-//                        {
-//                           "type":"click",
-//                           "name":"赞一下我们",
-//                           "key":"您好"
-//                        }]
-//                   },
-//                  {
-//                       "name":"测试",
-//                       "sub_button":[
-//                       {
-//                           "type":"view",
-//                           "name":"商城登录",
-//                           "url":"http://www.hfyiqiwan.top"
-//                        },
-//                        {
-//                           "type":"click",
-//                           "name":"了解我们",
-//                           "key":"您好"
-//                        }]
-//                   }]
-//             }';
-        $menuList=Common::http_request($url);
+        $menuList=Common::http_request($url);//https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxdfb8f36560c95e58&redirect_uri=http://www.hfyiqiwan.top/admin/login/Weixin/index&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect
         $menuList=json_decode($menuList,true);
-//        dump($menuList);exit;
         $this->assign("count",count($menuList["menu"]["button"]));
         $this->assign("menuList",$menuList["menu"]["button"]);
         return $this->fetch();
@@ -125,8 +85,7 @@ class Createmenu extends Common
         }
         $MenuList=$zhongjian;
         $createMenu["button"]=$MenuList;
-        $createMenu=json_encode($createMenu,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);//JSON_UNESCAPED_UNICODE
-//        dump($createMenu);exit;
+        $createMenu=json_encode($createMenu,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
         $cacessToken=Common::getAccessToken();
         $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=$cacessToken";
         $result=Common::http_request($url,"post",$createMenu);
