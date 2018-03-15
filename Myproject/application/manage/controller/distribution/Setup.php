88790524b7c60650d824773d5a0df3e893534506
shenrenhai <1294544527@qq.com>
2018-03-15 18:36:10 +0800

    分销设置

--- Myproject/application/manage/controller/distribution/Setup.php
+++ Myproject/application/manage/controller/distribution/Setup.php
@@ -7,4 +7,37 @@ class Setup extends Common
     {
         return $this->fetch();
     }
+
+    public function SaveInfo()
+    {
+        $info=input("data");
+        $info=json_decode($info,true);
+        $val=array();
+        foreach ($info as $value)
+        {
+            $value=json_decode(Common::fisker_decode_v2($value),true);
+            $arr=array();
+            foreach ($value as $val1)
+            {
+                $arr[$val1["name"]]=$val1["value"];
+            }
+            $val[]=$arr;
+        }
+
+        foreach ($val as $value1)
+        {
+            if($value1["IsPay"]==1 || $value1["IsPay"]==0)
+            {
+                if($value1["IsPay"]==1)
+                {
+                    $str = $value1["Price"];
+                    $isMatched = preg_match('/^(?:[1-9][0-9]*(?:\.[0-9]+)?|0\.(?!0+$)[0-9]+)$/', $str, $matches);
+                    var_dump($isMatched, $matches);
+                }
+            }
+        }
+
+
+        dump($val);
+    }
 }
---
+++ Myproject/public/static/manage/js/distribution/Setup/index.js
@@ -0,0 +1,53 @@
+function panNum(num) {
+    var text=parseInt(num);
+    if(!(text>=0))
+    {
+        $(".DayNum").val("");
+    }
+}
+
+function panPrice(float) {
+    var pattern = /^(?:[1-9][0-9]*(?:\.[0-9]+)?|0\.(?!0+$)[0-9]+)$/;
+    var type=pattern.test(float);
+    if(type==false)
+    {
+        $(".Price").val("");
+    }
+}
+
+$(function () {
+   $(".ShowPriceBox").click(function () {
+       var num=$(this).val();
+       if(num=="1")
+       {
+           $(this).parent("p").nextAll(".Price").fadeIn(100);
+       }
+       else
+       {
+           $(this).parent("p").nextAll(".Price").fadeOut(100);
+       }
+   });
+});
+
+function Save() {
+    var num=1;
+    var arr = {};
+    $(".distribution_Setup").each(function(){
+        arr[num-1] = fisker_encode_v2(JSON.stringify($("#distribution"+num).serializeArray()));
+        num++;
+    });
+    arr=JSON.stringify(arr);
+    $.ajax({
+        url:'SaveInfo',
+        type:'post',
+        data:({
+            data:arr
+        }),
+        success:function (msg) {
+            
+        },
+        error:function () {
+            
+        }
+    });
+}
