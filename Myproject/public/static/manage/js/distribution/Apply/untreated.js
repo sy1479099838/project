06f56e6fc7d2d938476287d2c2fd23741892b855
shenrenhai <1294544527@qq.com>
2018-03-20 11:38:57 +0800

    弹窗

--- Myproject/public/static/manage/js/distribution/Apply/untreated.js
+++ Myproject/public/static/manage/js/distribution/Apply/untreated.js
@@ -1,9 +1,29 @@
 $(function () {
     box.onclick = function(){
-        // var WinWidth="";
-        var winWidth = window.innerWidth;
-        alert(winWidth);
-        var myWindow=window.open("test.html",'newwindow','height=500,width=400,top=300,left=600,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no,status=no,z-look=yes');
-
+        var Width=window.innerWidth;
+        var Height=window.innerHeight;
+        var Awidth=300;
+        var Aheight=300;
+        var top=(Height-Aheight)/2;
+        var left=(Width-Awidth)/2;
+        myWindow=window.open("test.html",'_blank','height='+Aheight+',width='+Awidth+',top='+top+',left='+left+',toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no,status=no,z-look=yes');
+        var CantClick=$(".CantClick");
+        CantClick.attr("onclick","test12334()");
+        CantClick.fadeIn(0);
+        var loop = setInterval(function() {
+            if(myWindow .closed) {
+                clearInterval(loop);
+                var CantClick=$(".CantClick");
+                CantClick.attr("onclick","adsdfgg()");
+                CantClick.fadeOut(0);
+            }
+        }, 1000);
     };
 });
+
+function test12334() {
+    if(myWindow != null)
+    {
+        myWindow.focus();
+    }
+}
